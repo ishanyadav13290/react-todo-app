74304509026e1235d35d772b84109adcc519c5a4
ishanyadav13290 <ishanyadav13290@gmail.com>
2023-01-13 14:00:22 +0530

    footer shifting bug fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 function Footer(){
     return (
         <div className="Footer">
-       <h2>Made with React</h2>
        <div>
+       <h2>Made with React</h2>
         <a href="https://github.com/ishanyadav13290" target={"_"}><i class="fa-brands fa-github"></i></a>
         <a href="https://www.linkedin.com/in/ishan-yadav-5306851ba/" target={"_"}><i class="fa-brands fa-linkedin"></i></a>
        </div>
