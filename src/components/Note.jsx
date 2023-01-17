8166fba0699763ea202e097afeb8fe8bb019c0a6
ishanyadav13290 <ishanyadav13290@gmail.com>
2023-01-17 11:39:12 +0530

    note button styled

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,4 +1,11 @@
 function Note(props){
+    let style={
+        background:"transparent",
+        padding:"3px",
+        color:"white",
+        border:"1px solid white",
+        cursor:"pointer"
+    }
     function handleClick(){
         props.onDelete(props.id)
     }
@@ -7,7 +14,7 @@ function Note(props){
             <h1>{props.title}</h1>
             <hr />
             <p>{props.content}</p>
-            <button onClick={handleClick}>Delete</button>
+            <button onClick={handleClick} style={style}>Delete</button>
         </div>
     </>
 }
